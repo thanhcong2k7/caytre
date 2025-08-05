9dbb0db0a425f53c6ccd66d8c1098f203e439439
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-08-05 23:00:41 +0700

    favicon

--- app/index.php
+++ app/index.php
@@ -9,7 +9,7 @@ $_SESSION["chat_history"]=[];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lịch Vạn Niên CayTre - Tư vấn phong thủy & cuộc sống</title>
-
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
